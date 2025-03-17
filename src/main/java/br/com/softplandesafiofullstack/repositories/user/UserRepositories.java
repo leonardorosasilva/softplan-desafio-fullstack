4ec8aa48d4e2be342dcca1dbbd191d34d633e1ba
leonardorosasilva <03466316022@edu.udesc.br>
2025-03-17 14:59:06 -0300

    fix:: nome variavel

--- src/main/java/br/com/softplandesafiofullstack/repositories/user/UserRepositories.java
+++ src/main/java/br/com/softplandesafiofullstack/repositories/user/UserRepositories.java
@@ -1,11 +1,12 @@
 package br.com.softplandesafiofullstack.repositories.user;
 
 import br.com.softplandesafiofullstack.models.user.UserEntity;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepositories extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByLogin(String login);
+    Optional<UserEntity> findByEmail(@Email String email);
 }
