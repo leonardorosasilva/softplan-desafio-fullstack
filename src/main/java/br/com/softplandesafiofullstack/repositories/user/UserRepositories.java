22aa4f07d081ac88d4d56a53dd661fc0ba6c72a1
leonardorosasilva <03466316022@edu.udesc.br>
2025-03-19 10:44:48 -0300

    .

--- src/main/java/br/com/softplandesafiofullstack/repositories/user/UserRepositories.java
+++ src/main/java/br/com/softplandesafiofullstack/repositories/user/UserRepositories.java
@@ -3,10 +3,12 @@ package br.com.softplandesafiofullstack.repositories.user;
 import br.com.softplandesafiofullstack.models.user.UserEntity;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepositories extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(@Email String email);
 }
